0f9a4f3de48c0e6dcb3cb6ea557d0cf2d56d6886
vikasvks90 <vikas.varshney90@gmail.com>
2017-06-21 21:05:56 +0530

    Add files via upload

---
+++ Task3.java
@@ -0,0 +1,39 @@
+/**
+ * <h1>Task3</h1>
+ * This class will be used to define the configuration components for the given task
+ * */
+package mapreduce.assignment4.task3;
+
+import org.apache.hadoop.fs.Path; 
+import org.apache.hadoop.conf.*;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat; 
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat; 
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat; 
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+
+public class Task3 {
+	public static void main(String[] args) throws Exception {
+		Configuration conf = new Configuration();
+		Job job = new Job(conf, "Task3");
+		job.setJarByClass(Task3.class);
+
+		job.setMapOutputKeyClass(Text.class);
+		job.setMapOutputValueClass(IntWritable.class);
+
+		job.setOutputKeyClass(Text.class);
+		job.setOutputValueClass(IntWritable.class);
+		job.setMapperClass(Task3Mapper.class);
+		job.setReducerClass(Task3Reducer.class);
+		 
+		job.setInputFormatClass(TextInputFormat.class);
+		job.setOutputFormatClass(TextOutputFormat.class);
+
+		FileInputFormat.addInputPath(job, new Path(args[0])); 
+		FileOutputFormat.setOutputPath(job,new Path(args[1]));
+		
+		job.waitForCompletion(true);
+	}
+}
---
+++ Task3Reducer.java
@@ -0,0 +1,31 @@
+/**
+ * <h1>Task3Reducer</h1>
+ * Reducer program calculate the total units sold in each state for Onida company
+ * This class will take input as (Key,Value) pair from output of mapper class
+ * value will be a combined list for all the values for a given key
+ * */
+package mapreduce.assignment4.task3;
+
+import java.io.IOException;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+public class Task3Reducer extends Reducer<Text, IntWritable, Text, IntWritable>
+{
+	IntWritable outValue = new IntWritable();
+	
+	public void reduce(Text key, Iterable<IntWritable> values,Context context) throws IOException, InterruptedException
+	{
+		//we are taking the mapper class output and adding up all the values
+		//to get the total units sold in each state for "ONIDA" company
+		int sum = 0;
+		for (IntWritable value : values) {
+			sum += value.get();
+		}
+		
+		outValue.set(sum);
+		context.write(key, outValue);
+	}
+}
